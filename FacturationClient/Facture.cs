f65722f0158231f78258fcfb67a285a1936d8913
DocteurJames <sir.wildjustice@gmail.com>
2020-12-10 02:54:17 +0100

    1st commit

---
+++ FacturationClient/Facture.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace FacturationClient
+{
+    public class Facture
+    {
+        public string nomClient { get; set; }
+
+        public int numFacture { get; set; }
+       
+        public DateTime dateEmission { get; set; }
+        
+        public DateTime dateReglementAttendu { get; set; }
+        
+        public double montantDu { get; set; }
+        
+        public double montantRegle { get; set; }
+
+        public Facture(string p_nomClient, int p_numFacture, DateTime p_dateEmission, DateTime p_dateReglementAttendu, double p_montantDu, double p_montantRegle)
+        {
+            nomClient = p_nomClient;
+            numFacture = p_numFacture;
+            dateEmission = p_dateEmission;
+            dateReglementAttendu = p_dateReglementAttendu;
+            montantDu = p_montantDu;
+            montantRegle = p_montantRegle;
+        }
+    }
+}
---
+++ FacturationClient/Manager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace FacturationClient
+{
+    public class Manager
+    {
+        public List<Facture> listFactures;
+        public double totalAPayer = 0;
+        public double totalPaye = 0;
+        public Manager(int nbFactures)
+        {
+            listFactures = new List<Facture>();
+            string[] clients = new[]
+            {
+                "Bool", "Kool", "Dool", "Cool", "Mool", "Rool", "Pool", "Hool", "Wool", "Zool", "Fool"
+            };
+            
+            for (int i = 0; i < nbFactures; i++)
+            {
+                var rng = new Random();
+                string nomClient = clients[rng.Next(clients.Length)];
+                DateTime dateEmission = DateTime.Now.AddDays(rng.Next(-90, 0));
+                DateTime dateReglementAttendu = dateEmission.AddDays(rng.Next(0, 90));
+                double montantDu = rng.Next(5432, 10000) + Math.Round(rng.NextDouble(), 2);
+                double montantRegle = rng.Next(0, Convert.ToInt32(montantDu) / 10) * 10;
+                listFactures.Add(new Facture(nomClient, i, dateEmission, dateReglementAttendu, montantDu,
+                    montantRegle));
+                totalAPayer += montantDu;
+                totalPaye += montantRegle;
+            }
+        }
+    }
+}
